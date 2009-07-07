d51f7f09823d4e4452587038705d9593981f39eb
wyrdvans <jason.schadel@gmail.com>
2009-07-06 22:53:29 -0400

    Added yasnippets and org-mode config.

--- my-init.el
+++ my-init.el
@@ -23,14 +23,12 @@
 ;; Custom functions, keybindings and aliases
 ;;----------------------------------------------------------------------
 
-(message "Loading functions, keybindings and aliases...")
-(defun tasks () (interactive) (find-file "~/Documents/Work/StatusFiles/tasks.org"))
-(defun emacs () (interactive) (find-file "~/.emacs"))
-
 ;;-----
 ;; Keybindings
 ;;-----
-(global-set-key [f5] 'tasks)
+(global-set-key (kbd "<f5>") 'my-org-todo)
+(global-set-key (kbd "<S-f5>") 'widen)
+
 (global-set-key (kbd "<f6> a") (lambda ()
                                  (interactive)
                                  (find-file "~/.aliasrc")))
@@ -38,38 +36,35 @@
                                  (interactive)
                                  (find-file (concat dotfiles-dir "my-init.el"))))
 
-(global-set-key (kbd "<f7> t") 'trac-wiki)
 (global-set-key (kbd "<f7> l") 'linum-mode)
-
-(global-set-key (kbd "C-<f7>") 'geben)
-(global-set-key (kbd "C-<kp-delete>") 'kill-word)
-;; Custom Key Bindings
-(global-set-key (kbd "<f12>") 'org-agenda)
-(global-set-key (kbd "<f5>") 'my-org-todo)
-(global-set-key (kbd "<S-f5>") 'widen)
-(global-set-key (kbd "<f7>") 'set-truncate-lines)
+(global-set-key (kbd "<f7> g") 'geben)
+(global-set-key (kbd "<f7> t") 'trac-wiki)
+(global-set-key (kbd "<f7> w") 'set-truncate-lines)
 (global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
-(global-set-key (kbd "<f9> b") 'bbdb)
-(global-set-key (kbd "<f9> c") 'calendar)
-(global-set-key (kbd "<f9> f") 'boxquote-insert-file)
-(global-set-key (kbd "<f9> g") 'gnus)
-(global-set-key (kbd "<f9> i") (lambda ()
-                                 (interactive)
-                                 (info "~/git/org-mode/doc/org.info")))
-(global-set-key (kbd "<f9> o") 'org-occur)
+;; (global-set-key (kbd "<f9> b") 'bbdb)
+;; (global-set-key (kbd "<f9> c") 'calendar)
+;; (global-set-key (kbd "<f9> f") 'boxquote-insert-file)
+;; (global-set-key (kbd "<f9> g") 'gnus)
+;; (global-set-key (kbd "<f9> i") (lambda ()
+;;                                  (interactive)
+;;                                  (info "~/git/org-mode/doc/org.info")))
+;; (global-set-key (kbd "<f9> o") 'org-occur)
 (global-set-key (kbd "<f9> r") 'boxquote-region)
 (global-set-key (kbd "<f9> u") (lambda ()
                                  (interactive)
                                  (untabify (point-min) (point-max))))
 (global-set-key (kbd "<f9> v") 'visible-mode)
 (global-set-key (kbd "C-<f9>") 'previous-buffer)
-(global-set-key (kbd "C-x n r") 'narrow-to-region)
 (global-set-key (kbd "C-<f10>") 'next-buffer)
-(global-set-key (kbd "<f11>") 'org-clock-goto)
-(global-set-key (kbd "C-<f11>") 'org-clock-in)
-(global-set-key (kbd "C-s-<f12>") 'my-save-then-publish)
+;; (global-set-key (kbd "<f11>") 'org-clock-goto)
+;; (global-set-key (kbd "C-<f11>") 'org-clock-in)
+(global-set-key (kbd "<f12>") 'org-agenda)
+;; (global-set-key (kbd "C-s-<f12>") 'my-save-then-publish)
+
+(global-set-key (kbd "C-<kp-delete>") 'kill-word)
 (global-set-key (kbd "C-M-r") 'org-remember)
-;(global-set-key (kbd "<C-tab>") 'bury-buffer)
+(global-set-key (kbd "C-x n r") 'narrow-to-region)
+
 
 ;;----------------------------------------------------------------------
 ;; System Specific settings
@@ -138,6 +133,129 @@
 (autoload 'trac-wiki "trac-wiki" "Trac wiki editing entry-point." t)
 ;;; - start orgstruct minor mode when trac-wiki is loaded.
 (add-hook 'trac-wiki-mode-hook 'turn-on-orgstruct)
+
+;;-----
+;; Org mode
+;; -http://orgmode.org
+;;-----
+(add-to-list 'load-path (concat dotfiles-dir "vendors/org-mode/lisp"))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+(require 'org-install)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-todo-keywords (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
+                                (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "PROJECT(P@)" "OPEN(O@)" "|" "CANCELLED(c@/!)"))))
+
+(setq org-todo-keyword-faces (quote (("TODO" :foreground "red" :weight bold)
+                                     ("STARTED" :foreground "blue" :weight bold)
+                                     ("DONE" :foreground "forest green" :weight bold)
+                                     ("WAITING" :foreground "orange" :weight bold)
+                                     ("SOMEDAY" :foreground "magenta" :weight bold)
+                                     ("CANCELLED" :foreground "forest green" :weight bold)
+                                     ("QUOTE" :foreground "red" :weight bold)
+                                     ("QUOTED" :foreground "magenta" :weight bold)
+                                     ("APPROVED" :foreground "forest green" :weight bold)
+                                     ("EXPIRED" :foreground "forest green" :weight bold)
+                                     ("REJECTED" :foreground "forest green" :weight bold)
+                                     ("OPEN" :foreground "blue" :weight bold)
+                                     ("PROJECT" :foreground "red" :weight bold))))
+
+(setq org-use-fast-todo-selection t)
+(setq org-cycle-separator-lines 0)
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t) ("NEXT"))
+              ("SOMEDAY" ("WAITING" . t))
+              (done ("NEXT") ("WAITING"))
+              ("TODO" ("WAITING") ("CANCELLED"))
+              ("STARTED" ("WAITING"))
+              ("PROJECT" ("CANCELLED") ("PROJECT" . t)))))
+
+(require 'remember)
+(org-remember-insinuate)
+
+;; (setq org-remember-store-without-prompt t)
+;; (setq org-remember-default-headline "Tasks")
+;; 3 remember templates for TODO tasks, Notes, and Phone calls
+(setq org-remember-templates (quote (("todo" ?t "* TODO %?
+  %u
+  %a" "~/Documents/org-files/tasks.org" bottom nil)
+                                     ("note" ?n "* %?                                        :NOTE:
+  %u
+  %a" nil bottom nil)
+                                     ("phone" ?p "* PHONE %:name - %:company -                :PHONE:
+  Contact Info: %a
+  %u
+  :CLOCK-IN:
+  %?" "~/Documents/org-files/phone.org" bottom nil))))
+
+; Use IDO for target completion
+(setq org-completion-use-ido t)
+
+; Targets include this file and any file contributing to the agenda - up to 5 levels deep
+(setq org-refile-targets (quote ((org-agenda-files :maxlevel . 5) (nil :maxlevel . 5))))
+
+; Targets start with the file name - allows creating level 1 tasks
+(setq org-refile-use-outline-path (quote file))
+
+; Targets complete in steps so we start with filename, TAB shows the next level of targets etc 
+(setq org-outline-path-complete-in-steps t)
+
+(setq org-agenda-custom-commands 
+      (quote (("P" "Projects" tags "/!PROJECT" ((org-use-tag-inheritance nil)))
+              ("s" "Started Tasks" todo "STARTED" ((org-agenda-todo-ignore-with-date nil)))
+              ("w" "Tasks waiting on something" tags "WAITING" ((org-use-tag-inheritance nil)))
+              ("r" "Refile New Notes and Tasks" tags "REFILE" ((org-agenda-todo-ignore-with-date nil)))
+              ("n" "Notes" tags "NOTES" nil))))
+
+; Tags with fast selection keys
+(setq org-tag-alist (quote ((:startgroup)
+                            ("@InTown" . ?t)
+                            ("@Work" . ?w)
+                            ("@Home" . ?h)
+                            (:endgroup)
+                            ("NEXT" . ?N)
+                            ("PROJECT" . ?P)
+                            ("WAITING" . ?W)
+                            ("HOME" . ?H)
+                            ("PLAY" . ?p)
+                            ("CANCELLED" . ?C))))
+
+; Allow setting single tags without the menu
+(setq org-fast-tag-selection-single-key (quote expert))
+
+; For tag searches ignore tasks with scheduled and deadline dates
+(setq org-agenda-tags-todo-honor-ignore-options t)
+
+;; Make TAB the yas trigger key in the org-mode-hook and turn on flyspell mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; yasnippet
+            ;; (make-variable-buffer-local 'yas/trigger-key)
+            ;; (setq yas/trigger-key [tab])
+            ;; (define-key yas/keymap [tab] 'yas/next-field-group)
+            ;; flyspell mode to spell check everywhere
+            ;; (flyspell-mode 1)
+            ))
+
+
+;;-----
+;; Periodic commands
+;;-----
+(run-at-time "00:59" 3600 'org-save-all-org-buffers)
+
+;;-----
+;; yasnippets
+;; - http://yasnippet.googlecode.com/svn/trunk/doc/index.html
+;;-----
+(add-to-list 'load-path (concat dotfiles-dir "vendors/yasnippets"))
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory (concat dotfiles-dir "vendors/yasnippets/snippets"))
+(yas/load-directory (concat dotfiles-dir "vendors/my-snippets"))
 
 ;;----------------------------------------------------------------------
 ;; Custom-set-faces
