03e3f527629ec718f5fc0384d96b799d9be22e0b
Jason Schadel <jasons@aweber.com>
2011-08-04 14:52:55 -0400

    Added init_puppet_mode.el

--- init_puppet_mode.el
+++ init_puppet_mode.el
@@ -1,3 +1,8 @@
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+(add-hook 'puppet-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook
+                      'delete-trailing-whitespace nil t)))
